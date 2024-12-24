c5b837a35f30402e857519c7deccf6b7ec76eaeb
Ali <ali.tradding021@gmail.com>
2024-12-24 15:29:55 +0330

    commit

--- app/ui/ordercontent.jsx
+++ app/ui/ordercontent.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
+import { Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import DataTable from './datatable';
 function OrdersPage() {
   return (
-    <Box sx={{ py: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+   <>
+    <Box sx={{ py: 4, display: 'flex', mx: 5, justifyContent:'space-between', borderBottom:'1px solid white' }} >
       <Typography variant="h6">Order Management</Typography>
-      <Typography>Here are your recent orders. Track and manage your purchases.</Typography>
+      <Button variant="contained" color='primary' sx={{display:'flex', justifyContent:'center' , alignItems:'center'}} className='*:mx-2
+       capitalize'> 
+        <AddIcon/>
+        new order</Button>
     </Box>
+    <Box sx={{m:2}}>
+      <DataTable/>
+    </Box>
+   </>
   );
 }
 
--- app/ui/profilepage.jsx
+++ app/ui/profilepage.jsx
@@ -1,7 +1,15 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 export default function Profilepage() {
   return (
-    <div>page profile</div>
+    <>
+    <Box sx={{fontSize:'30px', m:5, borderBottom:'2px solid white', p:5}} >
+      Profile
+    </Box>
+    <Box>
+      
+    </Box>
+    </>
   )
 }
